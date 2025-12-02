390821770254089580fda2abcfe0df537d20d16b
old_user_name <gratin829@yahoo.co.jp>
2025-12-02 23:21:11 +0900

    プロトタイプ宣言おっけー

--- 1201/complex.h
+++ 1201/complex.h
@@ -12,6 +12,14 @@
 // プロトタイプ宣言部
 //↓↓↓↓ 33xx XXXX 記述部(この範囲以外に追加しない)
 
+complex makeComp(double real, double image);    //複素構造体生成
+complex makeCompRT(double r, double theta);     //極形式から複素構造体生成
+complex addComp(complex a, complex b);          //加算
+complex subComp(complex a, complex b);          //減算
+complex cmulComp(complex a, double k);          //定数倍
+complex mulComp(complex a, complex b);          //乗算
+double getR2(complex a);                        //2乗
+complex conjComp(complex a);                    //共役複素数  
 
 
 //↑↑↑↑ ここまでを 33xx XXXX が記述(この範囲以外に追加しない)
@@ -20,6 +28,11 @@
 
 //↓↓↓↓ 33yy YYYY 記述部(この範囲以外に追加しない)
 
+complex invComp(complex a);                     //逆数  
+complex divComp(complex a, complex b);          //除算
+double getTheta(complex a);                     //なす角(radian)
+void printComp(complex a);                      //表示
+void printCompRT(complex a);                    //極形式で表示
 
 
 //↑↑↑↑ ここまでを 33yy YYYY が記述(この範囲以外に追加しない)
